b059e0d9b71ae34aa24429e61a280451293ee1b7
Allan <alngregory@gmail.com>
2026-02-24 20:44:56 -0300

    Criando as classes e os métodos

---
+++ TestesXUnitCobranca/TestesXUnitCobranca/Dominio/Models/Cobranca.cs
@@ -0,0 +1,11 @@
+﻿namespace TestesXUnitCobranca.Dominio.Models
+{
+    public class Cobranca
+    {
+        public decimal ValorOriginal { get; set; }
+        public int DiasAtraso { get; set; }
+        public decimal PercentualMulta { get; set; }
+        public decimal PercentualJurosDia { get; set; }
+        public decimal PercentualMaxDesconto { get; set; }
+    }
+}
---
+++ TestesXUnitCobranca/TestesXUnitCobranca/Dominio/Servicos/CalculadoraCobranca.cs
@@ -0,0 +1,39 @@
+﻿using TestesXUnitCobranca.Dominio.Models;
+
+namespace TestesXUnitCobranca.Dominio.Servicos
+{
+    public class CalculadoraCobranca
+    {
+        public decimal CalcularMulta(Cobranca cobranca)
+        {
+            if (cobranca.DiasAtraso <= 0)
+                return 0;
+
+            return cobranca.ValorOriginal * cobranca.PercentualMulta / 100;
+        }
+
+        public decimal CalcularJuros(Cobranca cobranca)
+        {
+            if (cobranca.DiasAtraso <= 0)
+                return 0;
+
+            return cobranca.ValorOriginal * cobranca.PercentualJurosDia / 100 * cobranca.DiasAtraso;
+        }
+
+        public decimal AplicarDesconto(decimal valorAtual, decimal percentualDesconto)
+        {
+            if (percentualDesconto < 0)
+                throw new ArgumentException("Desconto inválido");
+
+            return valorAtual - (valorAtual * percentualDesconto / 100);
+        }
+
+        public decimal CalcularValorAtualizado(Cobranca cobranca)
+        {
+            var multa = CalcularMulta(cobranca);
+            var juros = CalcularJuros(cobranca);
+
+            return cobranca.ValorOriginal + multa + juros;
+        }
+    }
+}
